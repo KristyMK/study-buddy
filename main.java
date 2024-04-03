86dddab376a700ffb1fc0d84b8e27db2e75c5722
KristyMK <165931676+KristyMK@users.noreply.github.com>
2024-04-03 19:19:27 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+  public class Main {
+  public static void main(String[] args) {    
+    //System.out.println("Hello world!");
+    // winter, spring, summer, autumn
+    // warm jacket, t-shirt, swimming suite, rain coat
+    var temperature = 6;
+
+    if (temperature <= 5 ) {
+      System.out.println("Wear a warm jacket!");
+    }
+    else if (temperature <= 15 ) {
+      System.out.println("Wear a jacket!");
+    }
+    else if (temperature <= 30) {
+      System.out.println("Wear a t-shirt!");
+    }
+    
+  }
+}
